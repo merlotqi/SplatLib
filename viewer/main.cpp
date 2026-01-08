c9ed52cc3f5d240c321f77f00bb657bc77ca2aa1
Merlot.Qi <merlotrain.mc@gmail.com>
2026-01-08 14:20:54 +0800

    add imgui to renderer splat data, that's only a simple viewer.

---
+++ viewer/main.cpp
@@ -0,0 +1,15 @@
+﻿
+#include "application.h"
+
+int main(int argc, char** argv) {
+  try {
+    Application app(1600, 900);
+    return app.run();
+
+  } catch (const std::exception& e) {
+    std::cerr << "Fatal error: " << e.what() << std::endl;
+    return -1;
+  }
+
+  return 0;
+}
